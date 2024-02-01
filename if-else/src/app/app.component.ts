9f99a6cdda0986e52348ed6f268a4fba8c87821b
nilesh-taware <36688481+nilesh-taware@users.noreply.github.com>
2024-01-31 18:33:13 -0800

    if-else toggle

---
+++ if-else/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  
+  title = 'Angular If Else Statement Example';
+  isActive = false;
+
+  toggleActive() {
+    this.isActive = !this.isActive;
+  }
+}
