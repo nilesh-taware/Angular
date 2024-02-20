b0a4858e65893a9466e43d9d48084bd9abaa71ba
nilesh-taware <36688481+nilesh-taware@users.noreply.github.com>
2024-02-20 08:14:14 +0530

    Template Forms

---
+++ tempform/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,FormsModule,CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'tempform';
+email: any;
+name: any;
+onSubmit(): void {
+    console.log('Name:', this.name);
+    console.log('Email:', this.email);
+    
+  }
+}
